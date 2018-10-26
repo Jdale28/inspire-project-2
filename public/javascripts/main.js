bd1589ffbe540e806bd0e5c20d0034ae82116245
Jordan Dale <jordan.dale2@gmail.com>
2018-10-26 18:48:34 -0400

    Clicking on emotion cycles available quotes and populates one

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,16 @@
 
 $('.emotionText').click(function (e){
   // console.log(e.target.id)
-  let quotes = $('.modal')
+  let quotes = $('.popup')
   let quote = []
+  // Click on a button, push quotes pertaining to that emotion into array
   for(let i = 0; i< quotes.length; i++){
-    if ($('.modal').get(i).className.split(' ')[2] === e.target.id){
-      quote.push($('.modal').get(i))
+    $(quotes[i]).addClass('disappear')
+    if ($('.popup').get(i).className.split(' ')[1] === e.target.id){
+      quote.push($('.popup').get(i))
     }
   }
-  console.log(quote)
-
+  // Randomize selected quotes pertaining to that emotion
+  let num = Math.floor(Math.random() * Math.floor(quote.length))
+  $(quote[num]).removeClass('disappear')
 })
