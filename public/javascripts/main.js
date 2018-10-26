ec3992a56c5854601aea0e634a54a323715d0cd8
Jordan Dale <jordan.dale2@gmail.com>
2018-10-26 19:03:08 -0400

    Working shuffle button

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,26 @@
-
+myId = ''
+let quotes = $('.popup')
+let quote = []
 $('.emotionText').click(function (e){
-  // console.log(e.target.id)
-  let quotes = $('.popup')
-  let quote = []
+  myId = e.target.id
   // Click on a button, push quotes pertaining to that emotion into array
   for(let i = 0; i< quotes.length; i++){
     $(quotes[i]).addClass('disappear')
-    if ($('.popup').get(i).className.split(' ')[1] === e.target.id){
+    if ($('.popup').get(i).className.split(' ')[1] === myId){
+      quote.push($('.popup').get(i))
+    }
+  }
+  // Randomize selected quotes pertaining to that emotion
+  let num = Math.floor(Math.random() * Math.floor(quote.length))
+  $(quote[num]).removeClass('disappear')
+})
+
+
+$('.shuffle').click(function (e){
+  // Click on a button, push quotes pertaining to that emotion into array
+  for(let i = 0; i< quotes.length; i++){
+    $(quotes[i]).addClass('disappear')
+    if ($('.popup').get(i).className.split(' ')[1] === myId){
       quote.push($('.popup').get(i))
     }
   }
